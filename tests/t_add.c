9c92f97c691d8951ea8d07d32402b23662c6e0a2
James Turner <jamesturner246@gmail.com>
2017-09-06 12:58:00 +0100

    Use MPFI intervals only to compare in testsuite.

--- tests/t_add.c
+++ tests/t_add.c
@@ -23,15 +23,15 @@
 
 int main (int argc, char *argv[])
 {
+#ifdef WITH_MPFI
     const mpfa_uint_t n_tests = 100000;
     const mpfa_prec_t prec = 53;
     const mpfa_prec_t prec_internal = 128;
 
-    mpfa_uint_t i, total_fail, t1_fail, t2_fail;
     mpfa_t a, b, c;
-    mpfr_t a_lo, a_hi;
-    mpfr_t b_lo, b_hi;
-    mpfr_t c_lo, c_hi;
+    mpfi_t ai, bi, ci;
+    mpfa_uint_t i, total_fail;
+    mpfa_uint_t t1_fail, t2_fail;
 
     // Init test.
     test_rand_init();
@@ -39,46 +39,12 @@ int main (int argc, char *argv[])
     mpfa_init2(a, prec);
     mpfa_init2(b, prec);
     mpfa_init2(c, prec);
-    mpfr_init2(a_lo, prec);
-    mpfr_init2(a_hi, prec);
-    mpfr_init2(b_lo, prec);
-    mpfr_init2(b_hi, prec);
-    mpfr_init2(c_lo, prec);
-    mpfr_init2(c_hi, prec);
-    total_fail = 0;
-
-    for (t1_fail = 0, i = 0; i < n_tests; i++) {
-        // Set random A.
-        test_rand_mpfa(a, TEST_RAND_SMALL);
-        mpfa_get_bounds(a_lo, a_hi, a);
-
-        // Set random B.
-        test_rand_mpfa(b, TEST_RAND_SMALL);
-        mpfa_get_bounds(b_lo, b_hi, b);
-
-        // Randomly share symbols.
-        //test_share_syms(a, b, 5);
-
-        // Compare MPFA result with MPFR bound results.
-        mpfa_add(c, a, b);
-        mpfr_add(c_lo, a_lo, b_lo, MPFR_RNDD);
-        mpfr_add(c_hi, a_hi, b_hi, MPFR_RNDU);
-        t1_fail += test_compare_bounds(c, c_lo, c_hi);
-    }
-
-    total_fail += t1_fail;
-    printf("Test one: %llu out of %llu failed.\n", t1_fail, n_tests);
-
-#ifdef WITH_MPFI
-    // Compare with the MPFI interval arithmetic library.
-    mpfi_t ai, bi, ci;
-
-    // Init test vars, and set working precision.
     mpfi_init2(ai, prec);
     mpfi_init2(bi, prec);
     mpfi_init2(ci, prec);
+    total_fail = 0;
 
-    for (t2_fail = 0, i = 0; i < n_tests; i++) {
+    for (t1_fail = 0, i = 0; i < n_tests; i++) {
         // Set random A.
         test_rand_mpfa(a, TEST_RAND_SMALL);
         mpfa_get_mpfi(ai, a);
@@ -93,34 +59,27 @@ int main (int argc, char *argv[])
         // Compare MPFA result with MPFI result.
         mpfa_add(c, a, b);
         mpfi_add(ci, ai, bi);
-        t2_fail += test_compare_mpfi(c, ci);
+        t1_fail += test_compare_mpfi(c, ci);
     }
 
-    total_fail += t2_fail;
-    printf("Test two: %llu out of %llu failed.\n", t2_fail, n_tests);
-
-    // Cleanup test vars.
-    mpfi_clear(ai);
-    mpfi_clear(bi);
-    mpfi_clear(ci);
-
-#else // WITH_MPFI
-    fprintf(stderr,
-            "Part of this test uses the MPFI interval arithmetic library.\n"
-            "Recompile with MPFI support enabled to run the full test.\n");
-#endif // WITH_MPFI
+    total_fail += t1_fail;
+    printf("Test one: %llu out of %llu failed.\n", t1_fail, n_tests);
 
     // Cleanup test.
     mpfa_clear(a);
     mpfa_clear(b);
     mpfa_clear(c);
-    mpfr_clear(a_lo);
-    mpfr_clear(a_hi);
-    mpfr_clear(b_lo);
-    mpfr_clear(b_hi);
-    mpfr_clear(c_lo);
-    mpfr_clear(c_hi);
+    mpfi_clear(ai);
+    mpfi_clear(bi);
+    mpfi_clear(ci);
     test_rand_clear();
     mpfr_free_cache();
     return total_fail > 0;
+
+#else // WITH_MPFI
+    fprintf(stderr,
+            "This test uses the MPFI interval arithmetic library.\n"
+            "Recompile with MPFI support enabled to run this test.\n");
+    return 77; // Exit code 77 skips this test.
+#endif // WITH_MPFI
 }
