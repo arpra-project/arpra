0cf6dfd071bb0d80ca91bd5dcc31f72826fc4935
James Turner <jamesturner246@gmail.com>
2017-09-25 11:31:57 +0100

    Refactored t_inv to use new refactored testsuite code.

--- tests/t_inv.c
+++ tests/t_inv.c
@@ -26,64 +26,32 @@ int main (int argc, char *argv[])
 #ifdef WITH_MPFI
     const mpfa_prec_t prec = 53;
     const mpfa_prec_t prec_internal = 128;
-    const mpfa_uint_t n_tests = 100000;
-
-    mpfa_t x_A, z_A;
-    mpfi_t x_I, z_I, z_AI;
-    mpfr_t rdiam_I, rdiam_AI, difference;
-    mpfa_uint_t i, n_fail, total_fail;
+    const mpfa_uint_t test_n = 100000;
+    mpfa_uint_t i, fail_n;
 
     // Init test.
+    test_fixture_init(prec, prec_internal);
     test_rand_init();
-    mpfa_set_internal_prec(prec_internal);
-    mpfa_init2(x_A, prec);
-    mpfa_init2(z_A, prec);
-    mpfi_init2(x_I, prec);
-    mpfi_init2(z_I, prec);
-    mpfi_init2(z_AI, prec);
-    mpfr_init2(rdiam_I, prec_internal);
-    mpfr_init2(rdiam_AI, prec_internal);
-    mpfr_init2(difference, prec_internal);
-    total_fail = 0;
-
-    // Run test.
     test_log_init("inv");
-    for (n_fail = 0, i = 0; i < n_tests; i++) {
-        // Set random x and y.
-        test_rand_mpfa(x_A, TEST_RAND_SMALL);
-        mpfa_get_mpfi(x_I, x_A);
+    fail_n = 0;
 
-        // Compute z.
-        mpfa_inv(z_A, x_A);
-        mpfi_inv(z_I, x_I);
-        mpfa_get_mpfi(z_AI, z_A);
-
-        // Compute and log z diameter difference.
-        mpfi_diam_rel(rdiam_AI, z_AI);
-        mpfi_diam_rel(rdiam_I, z_I);
-        mpfr_sub(difference, rdiam_I, rdiam_AI, MPFR_RNDN);
-        mpfr_out_str (test_log, 10, 100, difference, MPFR_RNDN);
-        fputs("\n", test_log);
-
-        // Compare results.
-        n_fail += !mpfi_is_inside(z_I, z_AI);
+    // Start test.
+    for (i = 0; i < test_n; i++) {
+        if (test_univariate_mpfi(mpfa_inv, mpfi_inv)) {
+            test_log_printf("#%lu: FAIL\n\n", i);
+            fail_n++;
+        }
+        else {
+            test_log_printf("#%lu: PASS\n\n", i);
+        }
     }
-    total_fail += n_fail;
-    printf("Test one: %llu out of %llu failed.\n", n_fail, n_tests);
-    test_log_clear();
 
     // Cleanup test.
-    mpfa_clear(x_A);
-    mpfa_clear(z_A);
-    mpfi_clear(x_I);
-    mpfi_clear(z_I);
-    mpfi_clear(z_AI);
-    mpfr_clear(rdiam_I);
-    mpfr_clear(rdiam_AI);
-    mpfr_clear(difference);
+    printf("%llu out of %llu failed.\n", fail_n, test_n);
+    test_fixture_clear();
     test_rand_clear();
-    mpfr_free_cache();
-    return total_fail > 0;
+    test_log_clear();
+    return fail_n > 0;
 
 #else // WITH_MPFI
     fprintf(stderr,
