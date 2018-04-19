c6dff74ba83640ebb2348d90a7b4fd02ee57f479
James Paul Turner <jamesturner246@gmail.com>
2018-04-19 16:01:49 +0100

    Add ode_euler functions except step.

--- src/ode_euler.c
+++ src/ode_euler.c
@@ -20,3 +20,64 @@
  */
 
 #include "arpra-impl.h"
+
+typedef struct euler_scratch_struct
+{
+    arpra_range k1;
+} euler_scratch;
+
+static void euler_init (arpra_ode_stepper *stepper, arpra_ode_system *system)
+{
+    arpra_uint i;
+    arpra_precision prec;
+    euler_scratch *scratch;
+
+    stepper->method = arpra_ode_euler;
+    stepper->system = system;
+    stepper->scratch = malloc(system->dims * sizeof(euler_scratch));
+    scratch = (euler_scratch *) stepper->scratch;
+    for (i = 0; i < system->dims; i++) {
+        prec = arpra_get_precision(&(system->x[i]));
+        arpra_init2(&(scratch[i].k1), prec);
+    }
+}
+
+static void euler_clear (arpra_ode_stepper *stepper)
+{
+    arpra_uint i;
+    euler_scratch *scratch;
+
+    scratch = (euler_scratch *) stepper->scratch;
+    for (i = 0; i < stepper->system->dims; i++) {
+        arpra_clear(&(scratch[i].k1));
+    }
+    free(scratch);
+}
+
+static void euler_reset (arpra_ode_stepper *stepper)
+{
+    arpra_uint i;
+    euler_scratch *scratch;
+
+    scratch = (euler_scratch *) stepper->scratch;
+    for (i = 0; i < stepper->system->dims; i++) {
+        arpra_set_zero(&(scratch[i].k1));
+    }
+}
+
+static void euler_step (arpra_ode_stepper *stepper, const arpra_range *h)
+{
+    arpra_uint i;
+    euler_scratch *scratch;
+
+}
+
+static const arpra_ode_method euler =
+{
+    .init = &euler_init,
+    .clear = &euler_clear,
+    .reset = &euler_reset,
+    .step = &euler_step
+};
+
+const arpra_ode_method *arpra_ode_euler = &euler;
